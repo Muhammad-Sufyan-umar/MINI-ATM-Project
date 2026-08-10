d8cc5b1be3977b920a570dba3782293f9108d343
Muhammad-Sufyan-umar <sufyanumer04@gmail.com>
2026-08-10 15:51:14 +0500

    Refactor ATM code structure: move menu display to a separate function and restore login prompt in the main flow

--- ATM.py
+++ ATM.py
@@ -1,6 +1,5 @@
 class Atm:
 	def __init__(self):
-		print("Scan your card and enter your pin code to login..")
 		self.pin=3245
 		self.balance=50000
 	
@@ -48,17 +47,23 @@ class Atm:
 	
 	
 		
-	
+def menu():
+	print("=====================ATM=====================")
+
+	print("1.Check balance.")
+	print("2. Deposit Money.")
+	print("3. Make a withdrawl..")
+	print("4. Change pin")
+	print("5.Exit. \n")
 
 def main():
+
 	obj=Atm()
+
 	while True:
-		print("1.Check balance.")
-		print("2. Deposit Money.")
-		print("3. Make a withdrawl..")
-		print("4. Change pin")
-		print("5.Exit. \n")
-	
+
+		menu()
+
 		choice=int(input("Enter choice 1- 5: "))
 		
 		if choice==1:
@@ -80,6 +85,7 @@ def main():
 
 obj=Atm()
 def Login():
+	print("Scan your card and enter your pin code to login..")
 	while True:
 		print("Type 0 for quit program ")
 		
